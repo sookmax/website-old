e5f937929714f92f09f4bbd0db8adb37173bf7aa
Sook <sukkyu.chung@gmail.com>
2022-10-02 04:09:22 +0900

    fix timezone issue

--- server-scripts/post.ts
+++ server-scripts/post.ts
@@ -7,6 +7,8 @@ const DATE_FORMAT: Intl.DateTimeFormatOptions = {
   year: "numeric",
   month: "short",
   day: "numeric",
+  // https://stackoverflow.com/a/44096051
+  timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 };
 
 export function getAllPostMetadata() {
