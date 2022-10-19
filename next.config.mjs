478d70977af4a0a29db57ac0feac09dd56eb8342
Sook <sook.chung@3i.ai>
2022-10-19 15:25:14 +0900

    possible fix for https://github.com/chartjs/Chart.js/issues/9390

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true, // https://github.com/chartjs/Chart.js/issues/10673
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   trailingSlash: true,
